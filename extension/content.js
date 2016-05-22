e6dab0098a5b0a4c44e5a04d8cb17f1e55afeddd
mustilica <ilhan.mstf@gmail.com>
2016-05-22 13:10:43 +0300

    First pages commit

---
+++ extension/content.js
@@ -0,0 +1,10 @@
+setInterval(function() {
+  $('._5x46').hide();
+  $('._ipp').hide();
+  $('._ipn').hide();
+  $('.fcg').hide();
+  $('._5pb8').hide();
+  $('.UFIImageBlockImage').hide();
+  $('.UFICommentActorName').text("");
+  $('.profileLink').text("");
+}, 200);
